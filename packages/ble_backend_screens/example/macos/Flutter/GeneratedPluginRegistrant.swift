91bbc0692c877da9226582514f378db023ad3782
vovagorodok <vovagorodok2@gmail.com>
2024-12-09 18:15:49 +0100

    Update screens example ble_backend_factory version

--- packages/ble_backend_screens/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/ble_backend_screens/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,13 +8,13 @@ import Foundation
 import bluetooth_low_energy_darwin
 import device_info_plus
 import package_info_plus
-import path_provider_foundation
+import universal_ble
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BluetoothLowEnergyDarwinPlugin.register(with: registry.registrar(forPlugin: "BluetoothLowEnergyDarwinPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UniversalBlePlugin.register(with: registry.registrar(forPlugin: "UniversalBlePlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
--- packages/ble_backend_screens/example/windows/flutter/generated_plugins.cmake
+++ packages/ble_backend_screens/example/windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   bluetooth_low_energy_windows
   permission_handler_windows
+  universal_ble
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
