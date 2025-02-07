4973f2c68e10b2ea36f42552cb818fe71647314e
vovagorodok <vovagorodok2@gmail.com>
2025-02-07 14:55:04 +0100

    Rename ble_backend_screens_example to BleBackendScreens

--- packages/ble_backend_screens/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/ble_backend_screens/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import bluetooth_low_energy_darwin
 import device_info_plus
 import package_info_plus
+import reactive_ble_mobile
 import universal_ble
 import wakelock_plus
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BluetoothLowEnergyDarwinPlugin.register(with: registry.registrar(forPlugin: "BluetoothLowEnergyDarwinPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  ReactiveBlePlugin.register(with: registry.registrar(forPlugin: "ReactiveBlePlugin"))
   UniversalBlePlugin.register(with: registry.registrar(forPlugin: "UniversalBlePlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
